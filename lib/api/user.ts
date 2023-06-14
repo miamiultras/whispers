332b059503fa43c100fdb1343711fac3290a5358
eeeyyy <26793642+miamiultras@users.noreply.github.com>
2023-06-14 16:53:31 +0200

    create user api

---
+++ lib/api/user.ts
@@ -0,0 +1,66 @@
+import clientPromise from "@/lib/mongodb";
+
+export interface UserProps {
+  name: string;
+  username: string;
+  email: string;
+  image: string;
+}
+
+export async function getUser(username: string): Promise<UserProps | null> {
+  const client = await clientPromise;
+  const collection = client.db("test").collection("users");
+  const results = await collection.findOne<UserProps>(
+    { username },
+    { projection: { _id: 0, emailVerified: 0 } }
+  );
+  if (results) {
+    return {
+      ...results,
+    };
+  } else {
+    return null;
+  }
+}
+
+export async function getFirstUser(): Promise<UserProps | null> {
+  const client = await clientPromise;
+  const collection = client.db("test").collection("users");
+  const results = await collection.findOne<UserProps>(
+    {},
+    {
+      projection: { _id: 0, emailVerified: 0 },
+    }
+  );
+  if (results) {
+    return {
+      ...results,
+    };
+  } else {
+    return null;
+  }
+}
+
+export async function getAllUsers(): Promise<UserProps[]> {
+  const client = await clientPromise;
+  const collection = client.db("test").collection("users");
+  return await collection
+    .aggregate<UserProps>([
+      {
+        $limit: 100,
+      },
+      {
+        //sort alphabetically
+        $sort: {
+          _id: 1,
+        },
+      },
+    ])
+    .toArray();
+}
+
+export async function getUserCount(): Promise<number> {
+  const client = await clientPromise;
+  const collection = client.db("test").collection("users");
+  return await collection.countDocuments();
+}
