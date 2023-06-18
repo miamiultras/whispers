db62328b1f7223f30671c4ef4be40fee078fe340
eeeyyy <26793642+miamiultras@users.noreply.github.com>
2023-06-18 22:57:05 +0200

    add login/logout to home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
-import Link from "next/link";
+import { getProviders } from "next-auth/react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
-export default function Home() {
+import SignIn from "./components/SignIn";
+import Card from "./components/Card";
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
   return (
-    <main className="flex min-h-screen flex-col items-start p-5">
-      <h1>whispers</h1>
-      <Link
-        className="bg-white text-black py-2 px-4 mt-2 rounded-sm"
-        href="/users"
-      >
-        Users
-      </Link>
+    <main className="flex min-h-screen flex-col items-center p-5">
+      <h1 className="text-5xl uppercase font-extrabold">whispers</h1>
+      <section className="mt-10">
+        {session && session.user?.name ? (
+          <Card userName={session.user?.name} />
+        ) : (
+          <SignIn />
+        )}
+      </section>
     </main>
   );
 }
+getProviders;
