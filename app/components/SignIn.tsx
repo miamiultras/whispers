8547056a8e74070621293027e71942dc57a8f77a
eeeyyy <26793642+miamiultras@users.noreply.github.com>
2023-06-20 13:05:02 +0200

    revamp homepage and refactor sign in

--- app/components/SignIn.tsx
+++ app/components/SignIn.tsx
@@ -1,15 +1,28 @@
 "use client";
 import { signIn } from "next-auth/react";
+import TypewriterText from "./TypewriterText";
 
 function SignIn() {
   return (
-    <button
-      onClick={() => signIn("google")}
-      type="button"
-      className="bg-white my-4 text-black p-4 rounded-lg"
-    >
-      Sign in with Google
-    </button>
+    <div className="bg-white p-8 border-2 border-black rounded-lg flex flex-col justify-between gap-6">
+      <h2 className="text-black">
+        <TypewriterText text="Hello Stranger, are you ready to join text-based adventure?" />
+      </h2>
+      <button
+        onClick={() => signIn("google")}
+        type="button"
+        className="bg-white text-black p-4 border-black border-2 border-b-8 border-l-8 skew-x-4"
+      >
+        Sign in with Google
+      </button>
+      <button
+        onClick={() => {}}
+        type="button"
+        className="bg-white text-black p-4 border-black border-2 border-b-8 border-l-8 skew-x-4"
+      >
+        Sign in with <span className="font-extrabold">Magic Link</span>
+      </button>
+    </div>
   );
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   const session = await getServerSession(authOptions);
   return (
     <main className="flex min-h-screen flex-col items-center p-5">
-      <h1 className="text-5xl uppercase font-extrabold">whispers</h1>
+      <h1 className="text-5xl uppercase font-extrabold skew-y-3 mt-10">whispers</h1>
       <section className="mt-10">
         {session && session.user?.name ? (
           <Card userName={session.user?.name} />
