a1c1ca8b5df07315ec9c92faad6e8a65461b408d
eeeyyy <26793642+miamiultras@users.noreply.github.com>
2023-06-22 11:07:10 +0200

    refactor navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,26 +1,37 @@
 import Link from "next/link";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import Image from "next/image";
+import SignOutButton from "./SignOutButton";
 
-function Navbar() {
+async function Navbar() {
+  const session = await getServerSession(authOptions);
   return (
     <nav className="z-10 w-full p-4 px-8 flex items-center justify-between text-black bg-white border-b-4 border-black">
       <Link
         href="/"
-        className="font-extrabold uppercase shadow-inner skew-y-3 shadow-black p-2"
+        className="font-extrabold uppercase shadow-inner skew-y-3 shadow-black py-2 px-4"
       >
         whispers
       </Link>
-      <ul className="flex items-center">
-        <li>
-          <Link href="/settings" className="font-semibold hover:underline">
-            Settings
-          </Link>
-        </li>
-        <li className="ml-6">
-          <Link href="/about" className="font-semibold hover:underline">
-            About
-          </Link>
-        </li>
-      </ul>
+      {session?.user && (
+        <ul className="flex items-center">
+          <li>
+            <Link href="/profile">
+              <Image
+                src={session.user.image!}
+                alt="User image"
+                width={38}
+                height={38}
+                className="border-black border-2 border-b-4 border-r-4"
+              />
+            </Link>
+          </li>
+          <li className="ml-6">
+            <SignOutButton />
+          </li>
+        </ul>
+      )}
     </nav>
   );
 }
